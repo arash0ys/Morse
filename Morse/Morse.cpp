0f79ecaf4086410fbeabbfa6bd75060a5db8efca
JurJurWell <arash.yasini@outlook.com>
2025-06-05 23:05:43 +0200

    The morse now has a function for dot and dash. Only N can be morsed for now

--- Morse/Morse.cpp
+++ Morse/Morse.cpp
@@ -1,17 +1,42 @@
 // Morse.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
 #include <Windows.h>
+#define U 200
+
+int pher = 0;
+int freq = 700;
 void beep(int dwDur)
 {
-	Beep(750, dwDur);
+	Beep(freq, dwDur);
 }
+void dot(int count) {
+
+	for (count; count > 0; count--)
+	{
+		beep(U);
+	}
+	
+}
+void D(int count) {
+
+	for (count; count > 0; count--)
+	{
+		beep(U * 3);
+	}
+
+}
+
 int main()
 {
 	while (1) {
-
-		getchar();
-		beep(500);
+		pher = getchar();
+		if (pher == 110 || pher == 78)
+		{
+			D(1);dot(1);
+		}
+		Sleep(3 * U);
    }
 }
