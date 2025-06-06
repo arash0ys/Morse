57e29f8b215d46e820579e389c0a81763fb15403
JurJurWell <arash.yasini@outlook.com>
2025-06-06 13:45:51 +0200

    Morse function is now created based on dash and dot function. You can try SOS.

--- Morse/Morse.cpp
+++ Morse/Morse.cpp
@@ -4,10 +4,10 @@
 
 #include <iostream>
 #include <Windows.h>
-#define U 200
+#define U 150
 
 int pher = 0;
-int freq = 700;
+int freq = 500;
 void beep(int dwDur)
 {
 	Beep(freq, dwDur);
@@ -17,10 +17,10 @@ void dot(int count) {
 	for (count; count > 0; count--)
 	{
 		beep(U);
-	}
-	
+	}	
 }
-void D(int count) {
+
+void dash(int count) {
 
 	for (count; count > 0; count--)
 	{
@@ -29,14 +29,44 @@ void D(int count) {
 
 }
 
+void Morse(int m1, int m2, int m3) {
+
+	if (m1 > 0)	
+		dot(m1);
+	
+	if (m1 < 0)
+		dash(-m1);
+	
+	if (m2 > 0)
+		dot(m2);
+
+	if (m2 < 0)
+		dash(-m2);
+
+	if (m3 > 0)
+		dot(m3);
+
+	if (m3 < 0)
+		dash(-m3);
+}
+
+
 int main()
 {
 	while (1) {
 		pher = getchar();
-		if (pher == 110 || pher == 78)
+		if (pher == 'N' || pher == 'n')
 		{
-			D(1);dot(1);
+			Morse(-1, 1, 0);
 		}
-		Sleep(3 * U);
+		if (pher == 'S' || pher == 's')
+		{
+			Morse(3, 0, 0);
+		}
+		if (pher == 'O' || pher == 'o')
+		{
+			Morse(-3, 0, 0);
+		}
+		Sleep(2 * U);
    }
 }
