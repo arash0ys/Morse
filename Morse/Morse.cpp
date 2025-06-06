4b74afa3eb8203ed9c318c7bbe177e087c7cb1da
JurJurWell <arash.yasini@outlook.com>
2025-06-06 21:59:52 +0200

    Haven't tested all letters yet but they seem to work.  it still pause for three units between same letters

--- Morse/Morse.cpp
+++ Morse/Morse.cpp
@@ -5,6 +5,34 @@
 #include <iostream>
 #include <Windows.h>
 #define U 150
+int Codes[26][5] = {
+	{'A', 'a', 1, -1, 0},
+	{'B', 'b', -1, 3, 0},
+	{'D', 'd', -1, 2, 0},
+	{'E', 'e', 1, 0, 0},
+	{'F', 'f', 2, -1, 1},
+	{'G', 'g', -2, 1, 0},
+	{'H', 'h', 4, 0, 0},
+	{'I', 'i', 2, 0, 0},
+	{'J', 'j', 1, -3, 0},
+	{'K', 'k', -1, 1, -1},
+	{'L', 'l', 1, -1, 2},
+	{'M', 'm', -2, 0, 0},
+	{'N', 'n', -1, 1, 0},
+	{'O', 'o', -3, 0, 0},
+	{'P', 'p', 1, -2, 1},
+	{'Q', 'q', -2, 1, -1},
+	{'R', 'r', 1, -1, 1},
+	{'S', 's', 3, 0, 0},
+	{'T', 't', -1, 0, 0},
+	{'U', 'u', 2, -1, 0},
+	{'V', 'v', 3, -1, 0},
+	{'W', 'w', 0, -2, 0},
+	{'X', 'x', -1, 2, -1},
+	{'Y', 'y', -1, 1, -2},
+	{'Z', 'z', -2, 2, 0}
+};
+
 
 int pher = 0;
 int freq = 500;
@@ -17,7 +45,7 @@ void dot(int count) {
 	for (count; count > 0; count--)
 	{
 		beep(U);
-	}	
+	}
 }
 
 void dash(int count) {
@@ -31,12 +59,12 @@ void dash(int count) {
 
 void Morse(int m1, int m2, int m3) {
 
-	if (m1 > 0)	
+	if (m1 > 0)
 		dot(m1);
-	
+
 	if (m1 < 0)
 		dash(-m1);
-	
+
 	if (m2 > 0)
 		dot(m2);
 
@@ -55,18 +83,35 @@ int main()
 {
 	while (1) {
 		pher = getchar();
-		if (pher == 'N' || pher == 'n')
+
+		for (int i = 0; i < 25; i++)
 		{
-			Morse(-1, 1, 0);
+			for (int j = 0; j < 2; j++)
+			{
+
+				if (pher == Codes[i][j])
+				{
+					Morse(Codes[i][2], Codes[i][3], Codes[i][4]);
+					Sleep(3 * U);
+					break;
+				}
+
+
+			}
 		}
-		if (pher == 'S' || pher == 's')
+
+		if (pher == 'c' || pher == 'C')
 		{
-			Morse(3, 0, 0);
+			Morse(-1, 1, -1); dot(1);
+			Sleep(3 * U);
 		}
-		if (pher == 'O' || pher == 'o')
+		
+		if (pher == ' ')
 		{
-			Morse(-3, 0, 0);
+			Sleep(7 * U);
 		}
-		Sleep(2 * U);
-   }
+
+
+
+	}
 }
